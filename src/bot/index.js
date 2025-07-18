84bf44352f32698c66c70f3c468c888774e6ada5
Dylan <dylan.acevedo2843@gmail.com>
2025-07-18 16:27:00 -0500

    feat:Configuracion de flujo de mensajes telegram

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,8 +1,22 @@
+// src/bot/index.js
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const { handleMessage } = require('./handlers');
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+
+console.log('🤖 Bot iniciado...');
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, '¡Hola! Elige una opción para comenzar:', {
+    reply_markup: {
+      keyboard: [['Entrada', 'Salida']],
+      resize_keyboard: true,
+      one_time_keyboard: false,
+    },
+  });
+});
 
 bot.on('message', (msg) => {
   handleMessage(bot, msg);
